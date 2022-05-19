1f36d179e3fb6c6dc2d7a06a52c53f5d32a9c14c
Brian Nyingambe <briannmbe@gmail.com>
2022-05-19 23:00:11 +0300

    add modules

---
+++ module1.py
@@ -0,0 +1,14 @@
+from requests import get
+from tkinter import Canvas
+
+
+def kanye():
+    canvas = Canvas()
+    quote_text = canvas.create_text(150, 207, text="Kanye's quote", width=250, font=("Arial", 20, "bold"),
+                                    fill="white")
+
+    response = get("https://api.kanye.rest")
+    response.raise_for_status()
+    data = response.json()
+    quote = data["quote"]
+    canvas.itemconfig(quote_text, text=quote)
